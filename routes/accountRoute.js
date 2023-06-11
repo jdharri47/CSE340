99416e274f9f0d8e3fafb8532b18311642fba7e2
jdharri47 <har20040@byui.edu>
2023-06-10 22:04:36 -0600

    assignment 4 commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 // Route for the login veiw
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -10,8 +11,21 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route for the registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-// Route to process the registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+// Process the login attempt
+router.post(
+    "/login",
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+)
 
 // export the router
 module.exports = router
