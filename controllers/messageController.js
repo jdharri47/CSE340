daa6efc68ebd6db33eb77bb32d5243ca8c2189df
jdharri47 <har20040@byui.edu>
2023-07-15 21:57:27 -0600

    final commit

---
+++ controllers/messageController.js
@@ -0,0 +1,4 @@
+const utilities = require("../utilities/")
+const model = require("../models/message-model")
+
+const messageController = {}
---
+++ routes/messageRoute.js
@@ -0,0 +1,4 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/messageController")
+const utilities = require("../utilities/")
